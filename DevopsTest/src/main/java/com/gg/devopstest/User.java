fba252a958873c06c0a93a3280b8843c7a73a48b
Savita <savitamuggannavar@gmail.com>
2026-05-08 15:15:31 +0100

    changes

--- DevopsTest/src/main/java/com/gg/devopstest/User.java
+++ DevopsTest/src/main/java/com/gg/devopstest/User.java
@@ -12,7 +12,7 @@ import lombok.*;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int Id;
+    int id;
     String name;
     String email;
     boolean active;
